9df23754b7031cef32328694954e2750539432c6
alexblaauw <alexblaauw44@gmail.com>
2022-03-10 14:03:00 +0100

    Updating IntelliJ Settings

--- src/com/gekkiewekkie/Main.java
+++ src/com/gekkiewekkie/Main.java
@@ -7,9 +7,10 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<String> choices = new ArrayList<String>();
+        ArrayList<String> choices = new ArrayList<>();
         choices.add("Test1");
         choices.add("Test2");
+
         MultipleLetterChoice choice = new MultipleLetterChoice("Trying testing", 2, choices);
         choice.initChoice();
         int i = choice.awaitResponse();
