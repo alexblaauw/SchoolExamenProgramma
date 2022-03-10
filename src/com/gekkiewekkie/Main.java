e84c83cfd05a30f9f98f99649ee08aac957dd6fc
alexblaauw <alexblaauw44@gmail.com>
2022-03-10 15:35:16 +0100

    Made MultipleNumberChoice have the option to turn the last item in the list into a 0, and this is purely visual

--- src/com/gekkiewekkie/Main.java
+++ src/com/gekkiewekkie/Main.java
@@ -1,16 +1,18 @@
 package com.gekkiewekkie;
 
 import com.gekkiewekkie.commandline.MultipleLetterChoice;
+import com.gekkiewekkie.commandline.MultipleNumberChoice;
 
 import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<String> choices = new ArrayList<String>();
+        ArrayList<String> choices = new ArrayList<>();
         choices.add("Test1");
         choices.add("Test2");
-        MultipleLetterChoice choice = new MultipleLetterChoice("Trying testing", 2, choices);
+
+        MultipleNumberChoice choice = new MultipleNumberChoice("Trying testing", 2, choices);
         choice.initChoice();
         int i = choice.awaitResponse();
         System.out.println("Index of answer provided: " + i);
