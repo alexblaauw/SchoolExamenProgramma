d1a10c056c1eda2fc72b012c8434e584a9386aac
Robin <21104964@student.hhs.nl>
2022-03-10 16:18:23 +0100

    Begonnen aan StudentenLijst

--- src/com/gekkiewekkie/student/Student.java
+++ src/com/gekkiewekkie/student/Student.java
@@ -1,5 +1,25 @@
 package com.gekkiewekkie.student;
 
 public class Student {
+    private String name;
+    private Integer studentNummer;
+    private Integer leeftijd;
 
+    public Student(String name, Integer studentNummer, Integer leeftijd) {
+        this.name = name;
+        this.studentNummer = studentNummer;
+        this.leeftijd = leeftijd;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getStudentNummer() {
+        return studentNummer;
+    }
+
+    public Integer getLeeftijd() {
+        return leeftijd;
+    }
 }
