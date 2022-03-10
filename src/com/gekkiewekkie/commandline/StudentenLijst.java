60f3dad24c211c26a81c9ad5624d07b66cf0e420
Robin <21104964@student.hhs.nl>
2022-03-10 15:57:27 +0100

    studentenlijst toegevoegd

---
+++ src/com/gekkiewekkie/commandline/StudentenLijst.java
@@ -0,0 +1,10 @@
+package com.gekkiewekkie.commandline;
+import java.util.ArrayList;
+
+public class StudentenLijst {
+    ArrayList<String> Studenten = new ArrayList<String>();
+    public static void StudentenLijst() {
+
+    }
+
+}
