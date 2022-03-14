eccaacc3481af7962c5b9835a9dabf8576efcba4
Bram Post <21139245@student.hhs.nl>
2022-03-14 15:02:13 +0100

    Made class vraag en examen

--- src/com/gekkiewekkie/Main.java
+++ src/com/gekkiewekkie/Main.java
@@ -8,18 +8,20 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<String> choices = new ArrayList<>();
-        choices.add("Test1");
-        choices.add("Test2");
+        //ArrayList<String> choices = new ArrayList<>();
+        //choices.add("Test1");
+        //choices.add("Test2");
 
-        MultipleNumberChoice choice = new MultipleNumberChoice("Trying testing", 2, choices);
-        choice.initChoice();
-        int i = choice.awaitResponse();
-        System.out.println("Index of answer provided: " + i);
+        //MultipleNumberChoice choice = new MultipleNumberChoice("Trying testing", 2, choices);
+        //choice.initChoice();
+        //int i = choice.awaitResponse();
+        //System.out.println("Index of answer provided: " + i);
 
         // Voorbeeld van hoe je een interface presenteert in dit programma:
-        MainInterface mainInterface = new MainInterface();
-        mainInterface.runInterface();
+        //MainInterface mainInterface = new MainInterface();
+        //mainInterface.runInterface();
         // De runInterface functie kan uitgevuld worden met een stel print statements, multiple choices, etc.
+        Examen1 examen1 = new Examen1(3, 0);
+        examen1.startExamen();
     }
 }
