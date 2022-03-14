cf99ba35d003ae4c86d9a50766a162c6e8d1d108
alexblaauw <alexblaauw44@gmail.com>
2022-03-14 15:16:25 +0100

    Large maven refactoring

---
+++ src/main/java/com/gekkiewekkie/examen/ResultIO.java
@@ -0,0 +1,10 @@
+package com.gekkiewekkie.examen;
+
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ResultIO {
+    GsonBuilder gsonBuilder;
+}
