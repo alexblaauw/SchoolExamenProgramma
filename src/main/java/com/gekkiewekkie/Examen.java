1ff6da5643dfcd96ed253e466be2308ae757ee5f
alexblaauw <alexblaauw44@gmail.com>
2022-03-21 15:03:25 +0100

    Examen1() leeg voor testen

--- src/main/java/com/gekkiewekkie/Examen.java
+++ src/main/java/com/gekkiewekkie/Examen.java
@@ -14,6 +14,79 @@ class Examen {
     }
 
     public void Examen1() {
+
+        ResultIOHandler ioHandler = new ResultIOHandler();
+        ArrayList<Integer> exam1Answers = new ArrayList<>();
+        System.out.println("Welkom bij examen nummer 1, dit examen bestaat uit 3 vragen. Veel succes!");
+
+
+        System.out.println("Vraag 1:");
+        MultipleNumberChoice choice1;
+        choice1 = new MultipleNumberChoice("Wanneer mag je over een doorgetrokken streep rijden?",
+                "Altijd",
+                "Alleen als er aan jouw kant een niet-doorgetrokken streep er voor staat",
+                "Nooit");
+        choice1.initChoice();
+        int answer = choice1.awaitResponse();
+        int correctAnswer = 1;
+
+        if (answer == correctAnswer) {
+            totaalScore++;
+            System.out.println("Goed");
+        }
+        else {
+            System.out.println("Fout");
+        }
+        System.out.println("Het juiste antwoord is: " + (correctAnswer + 1));
+        System.out.println("Totaalscore: " + totaalScore + "\n");
+        exam1Answers.add(answer);
+
+
+        System.out.println("Vraag 2:");
+        MultipleNumberChoice choice2;
+        choice2 = new MultipleNumberChoice("Wanneer is er meer slipgevaar op de weg na een droge periode?",
+                "Als het net begonnen is met regenen",
+                "Als het al uren aan het regenen is");
+        choice2.initChoice();
+        answer = choice2.awaitResponse();
+        correctAnswer = 0;
+
+        if (answer == correctAnswer) {
+            totaalScore++;
+            System.out.println("Goed");
+        }
+        else {
+            System.out.println("Fout");
+        }
+        System.out.println("Het juiste antwoord is: " + (correctAnswer + 1));
+        System.out.println("Totaalscore: " + totaalScore + "\n");
+        exam1Answers.add(answer);
+
+
+        ArrayList<String> choices3 = new ArrayList<>();
+        choices3.add("Alle verkeersdeelnemers");
+        choices3.add("Alle bestuurders");
+        choices3.add("Alle voertuigen met een motor");
+
+        System.out.println("Vraag 3:");
+
+        MultipleNumberChoice choice3;
+        choice3 = new MultipleNumberChoice("Wie moet je voor laten bij haaientanden (als je recht door gaat)?", 3, choices3);
+        choice3.initChoice();
+        answer = choice3.awaitResponse();
+        correctAnswer = 1;
+
+        if (answer == correctAnswer) {
+            totaalScore++;
+            System.out.println("Goed");
+        }
+        else {
+            System.out.println("Fout");
+        }
+        System.out.println("Het juiste antwoord is: " + (correctAnswer + 1));
+        System.out.println("Totaalscore: " + totaalScore + "\n");
+        exam1Answers.add(answer);
+        ioHandler.saveArrayList(exam1Answers, "src/main/resources/exam1answers.json");
     }
 
     public void Examen2() {
