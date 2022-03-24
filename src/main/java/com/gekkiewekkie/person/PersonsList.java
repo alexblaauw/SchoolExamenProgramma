9c251ac932bbcbef8f0aa3d8b15d067e28c3f634
Robin <21104964@student.hhs.nl>
2022-03-24 13:30:18 +0100

    addperson class weggehaald

--- src/main/java/com/gekkiewekkie/person/PersonsList.java
+++ src/main/java/com/gekkiewekkie/person/PersonsList.java
@@ -1,26 +1,24 @@
 package com.gekkiewekkie.person;
 
+
 import java.util.ArrayList;
 
 public class PersonsList {
     private static ArrayList<Person> personenLijst = new ArrayList<Person>();
 
-    public   static ArrayList<Person> StudentenLijst() {
+    public static void StudentenLijst() {
         personenLijst.add(new Teacher("Henk", 65));
-        personenLijst.add(new Teacher("Natasja", 28));
+        personenLijst.add(new Teacher("Natasja", 27));
         personenLijst.add(new Teacher("Jonas", 48));
-        personenLijst.add(new Teacher("Liesanne", 18));
-        personenLijst.add(new Teacher("Hendrik", 18));
+        personenLijst.add(new Teacher("Liesanne", 28));
+        personenLijst.add(new Teacher("Hendrik", 35));
 
 
-        personenLijst.add(new Student("Amalia", 18, 1234125));
-        personenLijst.add(new Student("Rob", 12304987, 18));
-        personenLijst.add(new Student("Ruud", 12304987, 18));
-        personenLijst.add(new Student("Loes", 12304987, 18));
-        personenLijst.add(new Student("Mees", 12304987, 18));
-        personenLijst.add(new Student("Annika", 12304987, 18));
-
-        return StudentenLijst();
+        personenLijst.add(new Student("Rob", 19, 2532452));
+        personenLijst.add(new Student("Ruud", 17, 1238495));
+        personenLijst.add(new Student("Loes", 20, 1298375));
+        personenLijst.add(new Student("Mees", 20, 3140584));
+        personenLijst.add(new Student("Annika", 18, 2348095));
 
 
     }
@@ -33,6 +31,19 @@ public class PersonsList {
 
         personenLijst.add(new Student(naam, leeftijd, studentNummer));
 
-    }
 
+    }
+    public static void printStudenten(){
+        for (int i = 0; i < personenLijst.size(); i++) {
+            //System.out.println(personenLijst.get(i).getClass().toString());
+            if (personenLijst.get(i).getClass().toString().equals("class com.gekkiewekkie.person.Student")){
+                System.out.println("gelukt" + personenLijst.get(i).getName());
+                //System.out.println("Naam: " + personenLijst.get(i).getName() + "\nLeeftijd: " + personenLijst.get(i).getLeeftijd()
+                // + "\nstudentNummer: " + personenLijst.get(i).getStudentNummer());
+            }
+
+        }
+    }
+    //PersonsList.StudentenLijst();
+    //PersonsList.printStudenten();
 }
