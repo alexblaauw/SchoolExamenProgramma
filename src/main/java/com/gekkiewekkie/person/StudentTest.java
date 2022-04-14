2e43db0bd647c2ae9d6066c159c438123bc3be47
milan <21104875@student.hhs.nl>
2022-04-14 20:32:52 +0200

    JUnit test verder

--- src/main/java/com/gekkiewekkie/person/StudentTest.java
+++ src/main/java/com/gekkiewekkie/person/StudentTest.java
@@ -20,31 +20,23 @@ public class StudentTest {
          */
 
         // Arrange
-        ArrayList<Student> testStudentLijst = new ArrayList<>();
-        testStudentLijst.add(new Student("Henk Hofwegen", 35, 21234409));
-        testStudentLijst.add(new Student("Natasja de Ven", 27, 2462652));
-        testStudentLijst.add(new Student("Jonas Abbing", 42, 2557052));
-        testStudentLijst.add(new Student("Liesanne Reinders", 28, 7560455));
-        testStudentLijst.add(new Student("Hendrik de Wit", 35, 2805852));
-        testStudentLijst.add(new Student("Lars Post", 19, 2532402));
-        testStudentLijst.add(new Student("Ruud van Heijningen", 17, 1238495));
-        testStudentLijst.add(new Student("Loes Houtkooper", 20, 1290375));
-        testStudentLijst.add(new Student("Mees Bakker", 20, 3100584));
-        testStudentLijst.add(new Student("Annika Blauw", 18, 2348095));
-
-        // Act
         StudentList.StudentenLijst();
+        ArrayList<Student> testStudentLijst = new ArrayList<>();
+        for (int i = 0; i < StudentList.getStudentLijst().size() ; i++) {
+            testStudentLijst.add(StudentList.getStudentLijst().get(i));
+        }
+        // Act
         ArrayList<Student> result = StudentList.getStudentLijst();
 
         // Assert
-        assertTrue(testStudentLijst.equals(result));
+        assertEquals(testStudentLijst, result);
     }
 
     @Test
     public void testRemoveStudent() {
         /**
          * testRemoveStudent test de methode removeStudent waarin een student verwijdert wordt. Hier wordt dus getest
-         * dat student Henk Hofwegen met leeftijd 35 en studentNummer 21234409 verwijdert wordt uit de studentenlijst.
+         * dat student Henk Hofwegen met leeftijd 35 en studentnummer 21234409 verwijdert wordt uit de studentenlijst.
          * Als de test uitgevoerd wordt, wordt eerst de student verwijdert, hierna kijkt de test of de student
          * daadwerkelijk nog in de studentenlijst staat en als dat niet zo is, is de student correct en volledig
          * verwijdert.
@@ -64,7 +56,10 @@ public class StudentTest {
     @Test
     public void testAddStudent(){
         /**
-         *
+         * testAddStudent test de methode addStudent waarin een nieuwe student toegevoegd wordt aan de studentenlijst.
+         * Er wordt dus getest dat Robin van Genderen met leeftijd 18 en studentnummer 12345987 toegevoegd kan worden
+         * in de studentenlijst. Als de test uitgevoerd wordt, wordt er een nieuwe student toegevoegd en als de
+         * studentenlijst de student bevat, is de student correct toegevoegd.
          */
 
         // Arrange
@@ -82,7 +77,7 @@ public class StudentTest {
     @Test
     void testGetStudentenByNummer() {
         /**
-         *
+         * testGetStudentenByNummer
          */
 
         // Arrange
@@ -99,16 +94,20 @@ public class StudentTest {
     @Test
     void testGetExamenLijst(){
         /**
-         *
+         * testGetExamenLijst
          */
 
         // Arrange
         ExamList.getExamenLijst();
-
+        ArrayList<Examen> testExamenLijst = new ArrayList<>();
+        for (int i = 0; i < ExamList.getExamenLijst().size() ; i++) {
+            testExamenLijst.add(ExamList.getExamenLijst().get(i));
+        }
         // Act
-        Examen.getExamenLijst();
+        ArrayList<Examen> result = ExamList.getExamenLijst();
 
         // Assert
+        assertEquals(testExamenLijst, result);
     }
 
     @Test
@@ -118,17 +117,18 @@ public class StudentTest {
          */
 
         // Arrange
-        CommandLineChoice[] vragen = new CommandLineChoice[1];
-        Integer[] antwoorden = new Integer[1];
-        vragen[0] = new MultipleLetterChoice("What is the average speed of an unladen Swallow?", "I don't know that!", "African or European?");
-        antwoorden[0] = 1;
-
-        Examen examen = new Examen("test_examen", vragen, antwoorden);
-        ExamList.addExamen(examen);
+        ExamList.getExamenLijst();
 
         // Act
+        CommandLineChoice[] vragen = new CommandLineChoice[1];
+        Integer[] antwoorden = new Integer[1];
+        vragen[0] = new MultipleLetterChoice("Test", "123", "321");
+        antwoorden[0] = 1;
+        Examen examen = new Examen("examenTest", vragen, antwoorden);
+        ExamList.addExamen(examen);
 
         // Assert
+        assertTrue(ExamList.getExamenLijst().contains(examen));
     }
 }
 
