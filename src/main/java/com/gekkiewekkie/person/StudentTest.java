81fef84d807ae5bf82de74f0d08f545ba356e4bf
milan <21104875@student.hhs.nl>
2022-04-14 15:38:09 +0200

    JUnit tests

--- src/main/java/com/gekkiewekkie/person/StudentTest.java
+++ src/main/java/com/gekkiewekkie/person/StudentTest.java
@@ -1,17 +1,107 @@
 package com.gekkiewekkie.person;
+import com.gekkiewekkie.exam.ExamList;
+import com.gekkiewekkie.exam.Examen;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StudentTest {
     @Test
     public void testRemoveStudent() {
-        assertFalse(StudentList.removeStudent("Pietje", 18, 23109043));
+        // Arrange
+        boolean expectedValue = true;
 
+        // Act
+        boolean result = StudentList.removeStudent("Pietje", 18, 23109043);
+
+        // Assert
+        assertEquals(expectedValue, result);
+        assertTrue(result);
+
+        //
     }
-    public void testAddStudent(){
-        StudentList.addStudent("Robin", 18, 21104964);
 
+    @Test
+    public void testAddStudent(){
+        // Arrange
+        Student student;
+        StudentList.addStudent("Robin", 18, 12345987);
+        student = StudentList.getStudentByNummer(12345987);
+
+        // Act
+
+        // Assert
+        assertTrue(StudentList.getStudentLijst().contains(student));
+
+        //...
+    }
+
+    @Test
+    public void testGetStudentenLijst(){
+        // Arrange
+        var expectedValue = "";
+
+        // Act
+        var result = StudentList.getStudentLijst();
+
+        // Assert
+        assertEquals(expectedValue, result);
+
+        //...
+    }
+
+    @Test
+    void testGetStudentenByNummer() {
+        // Arrange
+        var expectedValue = 1238495;
+
+        // Act
+        var result = StudentList.getStudentByNummer(1238495);
+
+        // Assert
+        assertEquals(expectedValue, result);
+
+        //
+    }
+
+    @Test
+    void testGetExamenLijst(){
+        // Arrange
+
+        // Act
+        Examen.getExamenLijst();
+
+        // Assert
+
+        //
+    }
+
+    @Test
+    void testAddExamen(){
+        // Arrange
+
+        // Act
+        ExamList.addExamen();
+
+        // Assert
+
+        //
+    }
+
+    @Test
+    void testPrintStudenten() {
+        //Arrange
+
+        // Act
+        StudentList.printStudenten();
+
+        // Assert
+
+        //
     }
 }
+
